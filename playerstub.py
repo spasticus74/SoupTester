dc8cba2c55faa2a7442e557086f3ffd1955d801f
Spasticus74 <spasticus74@gmail.com>
2019-12-05 14:03:29 +1100

    Added playerstub.py
    Added a bare bones player script that implements the required interface, but only moves randomly.

---
+++ playerstub.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python
+
+# Simple Soup10 agent example,
+# implements the required interface
+# Intended as an example/starting point for further development
+
+import getopt, sys, random
+
+version = '0.0.1'
+author = "Reza Babarahim|babarahim@mmsi.com"
+
+random.seed()
+
+actions = ["EAT", "UL", "U", "UR", "L", "R", "DL", "D", "DR"]
+
+# Ignore the input, and move in a random direction
+def getMove(environment):
+    act = actions[random.randrange(0,9)]
+    print act + "|" + actions[random.randrange(0,9)]
+
+# Returns the author string defined above
+def getAuthor():
+    print author
+
+# Returns the version string defined above
+def getVersion():
+    print version
+
+# Process the args provided
+options, remainder = getopt.getopt(sys.argv[1:], 'avm:', ['author', 'version', 'move=',])
+
+for opt, arg in options:
+    if opt in ('-a', '--author'):
+        getAuthor()
+    elif opt in ('-v', '--version'):
+        getVersion()
+    elif opt in ('-m', '--move'):
+        getMove(arg)
